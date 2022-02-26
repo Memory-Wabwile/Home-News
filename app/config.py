5909e79954c40ceba453ad029c15dcd54483d748
Memory-Wabwile <memorywabwile@gmail.com>
2022-02-26 18:52:58 +0300

    add API base url and make API requests

--- app/config.py
+++ app/config.py
@@ -2,6 +2,8 @@ class Config:
     '''
     General configuration parent class
     '''
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
+    # HEADLINES_URL = 'https://newsapi.org/v2/top-headlines?country=us&category={}apiKey={}'
     pass
 
 class ProdConfig(Config):
