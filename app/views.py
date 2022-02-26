dcea5d4bef6361d081092f7ce62fec8d5b65b146
Memory-Wabwile <memorywabwile@gmail.com>
2022-02-26 17:26:07 +0300

    create an articles file to display the articles

--- app/views.py
+++ app/views.py
@@ -9,3 +9,13 @@ def index():
     '''
     message = 'Hello World'
     return render_template('index.html' , message=message)
+
+
+@app.route('/article/<int:id>')
+def article(id):
+    '''
+    view news and returns the news details
+    '''
+    article = article_source(id)
+    return render_template('article.html' , articles = articles , id=id)
+        
