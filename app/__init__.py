d372b10526f189e6eae2e0fb4bfbb9c56bd44cb9
Memory-Wabwile <memorywabwile@gmail.com>
2022-02-27 16:56:53 +0300

    install bootstrap and initialize it in the init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from .config import DevConfig
 
 # Initializing application
@@ -7,5 +8,8 @@ app = Flask(__name__,instance_relative_config = True)
 #Setting up configuration
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
+
+#initializing the flask extension
+bootstrap = Bootstrap(app)
 
 from app import views
