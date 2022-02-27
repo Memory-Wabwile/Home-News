2f917e071b0406b1e1631b1c35478c304c5ee8b5
Memory-Wabwile <memorywabwile@gmail.com>
2022-02-27 13:10:48 +0300

    create a sources test file to test the behaviour of the source class

---
+++ app/sources_test.py
@@ -0,0 +1,20 @@
+import unittest
+from models import sources
+
+class SourceTest(unittest.TestCase):
+    '''
+    Test class to test the Sources class
+    '''
+
+    def setUp(self):
+        '''
+        setup method that will run before every test
+        '''
+        self.news_source = ('bbc-news' , 'BBC News' , 'Estonia, Latvia and Slovenia join Bulgaria, Poland, the Czech Republic and the UK in announcing curbs.' , 'http://www.bbc.co.uk/news/world-europe-60539303')
+    
+
+    def test_instance(self):
+        '''
+        method to check if news source is an instance of the article class
+        '''
+        self.assertTrue(isinstance(self.news_source, Source))
