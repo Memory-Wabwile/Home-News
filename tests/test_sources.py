50a4adc05aaa3eb1f26167a530d909afb050007f
Memory-Wabwile <memorywabwile@gmail.com>
2022-02-28 13:18:27 +0300

    create a requirements.txt file

--- tests/test_sources.py
+++ tests/test_sources.py
@@ -10,15 +10,13 @@ class SourceTest(unittest.TestCase):
         '''
         setup method that will run before every test
         '''
-        self.news_source = ('bbc-news' , 'BBC News' , 'Estonia, Latvia and Slovenia join Bulgaria, Poland, the Czech Republic and the UK in announcing curbs.' , 'http://www.bbc.co.uk/news/world-europe-60539303')
+        self.news_source = Source('bbc-news' , 'BBC News' , 'Estonia, Latvia and Slovenia join Bulgaria, Poland, the Czech Republic and the UK in announcing curbs.' , 'http://www.bbc.co.uk/news/world-europe-60539303')
     
 
     def test_instance(self):
         '''
         method to check if news source is an instance of the article class
         '''
-        self.assertTrue(isinstance(self.news_source, Source))
+        self.assertTrue(isinstance(self.news_source,Source))
 
 
-# if __name__ == '__main__':
-#     unittest.main()
